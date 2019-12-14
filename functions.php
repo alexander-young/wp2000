0e5071c93ce136a8cfda6513038e71fee5bd101d
Alex Young <alex.young@clearlink.com>
2019-12-14 14:07:15 -0700

    fixes prefix

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function wp2k_scripts() {
 
-	wp_enqueue_style( 'wp21-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'wp2k-style', get_stylesheet_uri() );
 
 }
-add_action( 'wp_enqueue_scripts', 'wp21_scripts' );
+add_action( 'wp_enqueue_scripts', 'wp2k_scripts' );
