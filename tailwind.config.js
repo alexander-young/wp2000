c715d3641e7c08ce471f1077f859aa1251b17935
Alex Young <alex.young@clearlink.com>
2019-12-17 18:55:38 -0700

    adds colors and fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
+	theme: {
+		extend: {
+			inset: {
+				'-16': '-4rem'
+			},
+			colors: {
+				primary: {
+					light: '#f29544',
+					default: '#F4821E',
+					dark: '#d66400'
+				}
+			},
+			fontFamily: {
+				'display': 'Lato, sans-serif',
+				'body': 'Lato, sans-serif'
+			}
+		}
+	}
+};
