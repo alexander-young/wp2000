e5ca13421bf4286c10e32d24ad023e5ba8844372
Alex Young <alex.young@clearlink.com>
2019-12-27 13:08:22 -0700

    moves cta to footer

--- footer.php
+++ footer.php
@@ -5,6 +5,15 @@ defined('ABSPATH') || exit;
 
 </div><!-- #page we need this extra closing tag here -->
 
+  <div class="w-full relative bg-cover bg-center px-8" style="background-image: url('https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=650&w=1940);">
+    <div class="absolute inset-0 bg-gradient-black"></div>
+    <div class="w-full max-w-2xl mx-auto relative bg-gray-100 text-center py-12 md:py-14 -bottom-16 shadow-2xl">
+      <h4 class="h2 mb-4">Ready To Start?</h4>
+      <p class="mb-8 px-8 max-w-xl mx-auto">Take your design to the next level with Miya Interiors’ full-service design process. Your space is an extension of who you are, why not design it through-and-through?</p>
+      <a href="/" class="button">Contact Us <i class="las la-arrow-right"></i></a>
+    </div>
+  </div>
+
 <div class="pt-32 pb-16 text-gray-100 bg-gray-800 text-center">
   <ul class="mb-4">
     <li class="inline-block"><i class="lab la-lg la-facebook-f"></i></li>
