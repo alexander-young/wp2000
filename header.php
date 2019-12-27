f5e90ce6d2800868d426e3a756179a588e4d661d
Alex Young <alex.young@clearlink.com>
2019-12-27 13:08:36 -0700

    responsive fixes to header

--- header.php
+++ header.php
@@ -18,25 +18,27 @@ defined('ABSPATH') || exit;
 
 	<div class="w-full shadow-lg py-3">
 
-		<div class="container flex justify-between md:justify-end py-2 text-gray-800 text-sm md:text-base">
+		<div class="container flex justify-between lg:justify-end py-2 text-gray-800 text-sm lg:text-base">
 			<a href="tel:123-456-7890" class="inline-block mr-8"><i class="las la-phone"></i> 123.456.7890</a>
 			<a href="mailto:me@example.com"><i class="hidden sm:inline-block las la-envelope"></i> me@example.com</a>
 		</div>
 
-		<div class="container flex items-center justify-between relative">
+		<div class="container">
+			<div class="inner-container flex items-center justify-between relative">
 
-			<h2>LOGO GOES HERE</h2>
+				<h2 class="column">LOGO GOES HERE</h2>
 
-			<nav class="main-menu hidden md:block">
-					<?php wp_nav_menu([
-						'theme_location' => 'main_menu',
-						'container_class' => '',
-					]); ?>
-			</nav>
+				<nav class="main-menu hidden lg:block column">
+						<?php wp_nav_menu([
+							'theme_location' => 'main_menu',
+							'container_class' => '',
+						]); ?>
+				</nav>
 
-			<button class="menu-toggle block md:hidden">
-				<i class="las la-2x la-bars"></i>
-			</button>
+				<button class="menu-toggle block lg:hidden column">
+					<i class="las la-2x la-bars"></i>
+				</button>
+			</div>
 
 		</div>
 
