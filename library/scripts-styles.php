cd6f9a6d4f869618f64aee51465baffaeb6edf37
Alex Young <alex.young@clearlink.com>
2020-01-03 14:56:12 -0700

    fixes vendor dependancy

--- library/scripts-styles.php
+++ library/scripts-styles.php
@@ -11,7 +11,7 @@ function wp2k_scripts() {
   wp_deregister_script('jquery');
 
   wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', [], '2.1.0', true);  
-  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/assets/dist/js/main.js', ['jquery'], '1.0.0', true );
+  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/assets/dist/js/main.js', ['jquery' ], '1.0.0', true );
 
 }
 add_action('wp_enqueue_scripts', 'wp2k_scripts');
